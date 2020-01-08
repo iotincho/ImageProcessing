377019906e0fa500d1314f54825bf557c83f9a45
martin <martin.barrera@outlook.com>
2020-01-08 11:32:44 -0300

    darknet modificado para opencv4

--- darknetMod/src/image_opencv.cpp
+++ darknetMod/src/image_opencv.cpp
@@ -8,7 +8,7 @@
 using namespace cv;
 
 extern "C" {
-
+/*
 IplImage *image_to_ipl(image im)
 {
     int x,y,c;
@@ -44,6 +44,7 @@ image ipl_to_image(IplImage* src)
     }
     return im;
 }
+*/
 
 Mat image_to_mat(image im)
 {
@@ -51,19 +52,67 @@ Mat image_to_mat(image im)
     constrain_image(copy);
     if(im.c == 3) rgbgr_image(copy);
 
-    IplImage *ipl = image_to_ipl(copy);
+/* modified in order to work with opencv4 */
+/* begin */
+    Mat m(cv::Size(im.w,im.h), CV_8UC(im.c));
+    int x,y,c;
+    int step = m.step;
+    for(y = 0; y < im.h; ++y){
+        for(x = 0; x < im.w; ++x){
+             for(c= 0; c < im.c; ++c){
+                 float val = im.data[c*im.h*im.w + y*im.w + x];
+                 m.data[y*step + x*im.c + c] = (unsigned char)(val*255);
+            }
+        }
+    }
+    free_image(copy);
+    return m;
+/* end */
+
+/* original code */
+/* begin */
+/*    IplImage *ipl = image_to_ipl(copy);
     Mat m = cvarrToMat(ipl, true);
     cvReleaseImage(&ipl);
     free_image(copy);
     return m;
+*/
+/* end */
+
 }
 
 image mat_to_image(Mat m)
 {
-    IplImage ipl = m;
+
+/* modified in order to work with opencv4 */
+/* begin */
+    int h = m.rows;
+    int w = m.cols;
+    int c = m.channels();
+    image im = make_image(w, h, c);
+    unsigned char *data = (unsigned char *)m.data;
+    int step = m.step;
+    int i, j, k;
+
+    for(i = 0; i < h; ++i){
+        for(k= 0; k < c; ++k){
+            for(j = 0; j < w; ++j){
+                im.data[k*w*h + i*w + j] = data[i*step + j*c + k]/255.;
+            }
+        }
+    } 
+    rgbgr_image(im);
+    return im;
+/* end */
+
+/* original code */
+/* begin */
+/*    IplImage ipl = m;
     image im = ipl_to_image(&ipl);
     rgbgr_image(im);
     return im;
+*/
+/* end */
 }
 
 void *open_video_stream(const char *f, int c, int w, int h, int fps)
@@ -72,9 +121,9 @@ void *open_video_stream(const char *f, int c, int w, int h, int fps)
     if(f) cap = new VideoCapture(f);
     else cap = new VideoCapture(c);
     if(!cap->isOpened()) return 0;
-    if(w) cap->set(CV_CAP_PROP_FRAME_WIDTH, w);
-    if(h) cap->set(CV_CAP_PROP_FRAME_HEIGHT, w);
-    if(fps) cap->set(CV_CAP_PROP_FPS, w);
+    if(w) cap->set(CAP_PROP_FRAME_WIDTH, w);
+    if(h) cap->set(CAP_PROP_FRAME_HEIGHT, w);
+    if(fps) cap->set(CAP_PROP_FPS, w);
     return (void *) cap;
 }
 
@@ -123,7 +172,7 @@ void make_window(char *name, int w, int h, int fullscreen)
 {
     namedWindow(name, WINDOW_NORMAL); 
     if (fullscreen) {
-        setWindowProperty(name, CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
+        setWindowProperty(name, WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
     } else {
         resizeWindow(name, w, h);
         if(strcmp(name, "Demo") == 0) moveWindow(name, 0, 0);
